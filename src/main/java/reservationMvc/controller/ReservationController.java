bf4895d084cbea94ba6359a232939345bf295898
unknown <sajidaayachi@gmail.com>
2019-11-25 12:49:04 +0100

    ajout sajida

---
+++ src/main/java/reservationMvc/controller/ReservationController.java
@@ -0,0 +1,68 @@
+package reservationMvc.controller;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import reservationSpring.model.Reservation;
+import reservationSpring.repository.ReservationRepository;
+
+@Controller
+@RequestMapping("/reservation")
+public class ReservationController {
+
+	
+	@Autowired
+	private ReservationRepository reservationRepository;
+	
+	@GetMapping("/listReservation")
+	public String list(Model model) {
+		model.addAttribute("reservations", reservationRepository.findAll());
+		return "reservation/listReservation";
+	}
+	@GetMapping("/delete")
+	public String delete(@RequestParam(name = "idReservation", required = true) Long idReservation) {
+		reservationRepository.deleteById(idReservation);;
+	return "redirect:/reservation/listReservation";
+      }
+
+	@GetMapping("/edit")
+	public String edit(@RequestParam(name = "idReservation", required = true) Long idReservation, Model model) {
+		Optional<Reservation> opt = reservationRepository.findById(idReservation);
+		Reservation r = null;
+		if (opt.isPresent()) {
+			r = opt.get();
+		} else {
+			r = new Reservation();
+		}
+		return goEdit(r, model);
+	}
+	
+	private String goEdit(Reservation reservation, Model model) {
+			model.addAttribute("reservation", reservation);
+		return "reservation/editReservation";
+	}
+
+	@GetMapping("/addReservation")
+	public String addReservation(Model model) {
+		return goEdit(new Reservation(), model);
+	}
+
+	@PostMapping("/saveReservation")
+		public String saveReservation(@ModelAttribute("reservation") Reservation reservation, Model model) {
+		return save(reservation, model);
+	}
+
+	private String save(Reservation reservation, Model model) {
+		reservationRepository.save(reservation);
+		return "redirect:/reservation/listReservation";
+		}
+
+}
---
+++ src/main/webapp/WEB-INF/pages/reservation/listReservation.jsp
@@ -0,0 +1,63 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+<%@taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<%@taglib prefix="fmt" uri="http://java.sun.com/jsp/jstl/fmt"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>liste des reservations</title>
+<link rel="stylesheet"
+	href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+<script
+	src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<script
+	src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+<script
+	src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+</head>
+<body>
+	<div class="container">
+		<h1>liste des reservations</h1>
+		<div>
+			<a href="addReservation" class="btn btn-link">ajouter reservation</a>
+		</div>
+
+		<table class=" table table-bordered table-striped">
+			<thead>
+				<tr>
+					<th>id</th>
+					<th>numero</th>
+					<th>date</th>
+
+					<th></th>
+					<th></th>
+				</tr>
+			</thead>
+			<tbody>
+				<c:forEach var="r" items="${reservations}">
+					<tr>
+						<td>${r.idReservation}</td>
+						<td>${r.numeroReservation}</td>
+						<td><fmt:formatDate value="${r.dateReservation}" pattern="dd/MM/yyyy"/></td>
+
+
+
+						<c:url var="edit" value="edit">
+							<c:param name="idReservation" value="${r.idReservation}"></c:param>
+						</c:url>
+
+						<td><a href="${edit}" class="btn btn-outline-primary">modifier</a>
+						</td>
+
+						<c:url var="delete" value="delete">
+							<c:param name="idReservation" value="${r.idReservation}"></c:param>
+						</c:url>
+						<td><a href="${delete}" class="btn btn-outline-danger">supprimer</a></td>
+					</tr>
+				</c:forEach>
+			</tbody>
+		</table>
+	</div>
+</body>
+</html>
